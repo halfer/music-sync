1da66dbe445fc3fd42b430dceea4697492514550
halfer <projects.ws@jondh.me.uk>
2021-03-27 10:12:19 +0000

    Some work on the recursive glob feature

--- src/Service/FileOperation/Directory.php
+++ src/Service/FileOperation/Directory.php
@@ -5,26 +5,48 @@ namespace MusicSync\Service\FileOperation;
 class Directory extends FsObject
 {
     protected array $contents = [];
+    protected bool $populated = false;
 
-    public function glob(string $pattern, $recursive = false)
+    public function glob(string $pattern = '*')
     {
         $this->clearContents();
         foreach (glob($pattern) as $path)
         {
+            $this->pushObject($path);
+        }
+        $this->setPopulated();
+    }
 
+    public function recursivePopulate()
+    {
+        foreach ($this->contents as $fsObject) {
+            if ($fsObject instanceof Directory) {
+                $path = $fsObject->getPath() .
+                    DIRECTORY_SEPARATOR .
+                    $fsObject->getName();
+                $fsObject->glob($path);
+                $fsObject->recursivePopulate();
+            }
         }
     }
 
     protected function pushObject(string $path)
     {
-        if (is_file($path))
-        {
-            $this->contents = new File($path);
+        if (is_file($path)) {
+            $this->contents[] = new File($path);
+        } elseif (is_dir($path)) {
+            $this->contents[] = new Directory($path);
         }
     }
 
     protected function clearContents()
     {
         $this->contents = [];
+        $this->populated = false;
+    }
+
+    protected function setPopulated()
+    {
+        $this->populated = true;
     }
 }
---
+++ tests/integration/GlobTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MusicSync\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class GlobTest extends TestCase
+{
+    public function testNonRecursiveGlob()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRecursiveGlob()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
