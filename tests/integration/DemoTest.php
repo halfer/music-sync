eabb597de677d0e5c36af532905bd746fca19209
halfer <projects.ws@jondh.me.uk>
2021-03-27 00:49:55 +0000

    Add dummy test structure

---
+++ tests/integration/DemoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MusicSync\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/unit/DemoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MusicSync\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->markTestIncomplete();
+    }
+}
