db42e53dee99ffdd86aa208a42ebcc4b99198518
halfer <projects.ws@jondh.me.uk>
2021-03-28 11:30:37 +0100

    Sketch out some directory sort code (untested)

--- src/Service/FileOperation/Directory.php
+++ src/Service/FileOperation/Directory.php
@@ -6,11 +6,12 @@ use RuntimeException;
 
 class Directory extends FsObject
 {
-    const POPULATE_OPTION_CONTENTS = 'contents';
-    const POPULATE_OPTION_SIZE = 'size';
+    const SORT_NAME = 'name';
+    const SORT_SIZE = 'size';
 
     protected array $contents = [];
     protected bool $populated = false;
+    protected bool $sortDirectionAscending = true;
 
     public function glob(string $pattern = '*')
     {
@@ -29,6 +30,7 @@ class Directory extends FsObject
 
     /**
      * Populates FS structure, and optionally gets file sizes too
+     *
      * @param bool $popSize
      */
     public function recursivePopulate(bool $popSize = false)
@@ -60,6 +62,71 @@ class Directory extends FsObject
         }
 
         return $this->contents;
+    }
+
+    public function sort(string $sortType, bool $ascending = true)
+    {
+        $this->setSortDirection($ascending);
+        usort($this->contents, [$this, $this->getSorter($sortType)]);
+    }
+
+    public function recursiveSort(string $sortType, bool $ascending = true)
+    {
+        foreach ($this->getContents() as $fsObject) {
+            if ($fsObject instanceof Directory) {
+                // TODO call recursive sorter here, passing in $fsObject
+            }
+        }
+    }
+
+    protected function getSorter(string $sortType)
+    {
+        switch ($sortType) {
+            case self::SORT_NAME:
+                $sorter = 'sortDeviceName';
+                break;
+            case self::SORT_SIZE:
+                $sorter = 'sortDeviceSize';
+                break;
+            default:
+                throw new RuntimeException('Invalid sort type');
+        }
+
+        return $sorter;
+    }
+
+    protected function sortDeviceName(FsObject $a, FsObject $b)
+    {
+        list($aSwapped, $bSwapped) = $this->getSwappedSortObjects($a, $b);
+
+        return $aSwapped->getName() <=> $bSwapped->getName();
+    }
+
+    protected function sortDeviceSize(FsObject $a, FsObject $b)
+    {
+        list($aSwapped, $bSwapped) = $this->getSwappedSortObjects($a, $b);
+
+        return $aSwapped->getSize() <=> $bSwapped->getSize();
+    }
+
+    /**
+     * Exchanges the supplied sort pair based on the prevailing sort direction
+     *
+     * @param FsObject $a
+     * @param FsObject $b
+     * @return FsObject[]
+     */
+    protected function getSwappedSortObjects(FsObject $a, FsObject $b)
+    {
+        $aSwapped = $this->sortDirectionAscending ? $a : $b;
+        $bSwapped = $this->sortDirectionAscending ? $b : $a;
+
+        return [$aSwapped, $bSwapped];
+    }
+
+    protected function setSortDirection(bool $ascending)
+    {
+        $this->sortDirectionAscending = $ascending;
     }
 
     protected function clearContents()
