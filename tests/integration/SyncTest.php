1815e93cba862d53106810bfc49c3c28b47bc0a7
halfer <projects.ws@jondh.me.uk>
2021-04-15 23:18:02 +0100

    Make a start on a new test class

---
+++ tests/integration/SyncTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MusicSync\Test\Integration;
+
+use MusicSync\Service\FileOperation\Directory;
+use MusicSync\Test\TestCase;
+
+class SyncTest extends TestCase
+{
+    public function testSimpleCase()
+    {
+        $this->markTestIncomplete();
+    }
+
+    protected function createStructure1()
+    {
+        $dir = new Directory();
+    }
+}
